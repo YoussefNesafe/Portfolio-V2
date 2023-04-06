803fe30717c9729c3fa9638406381d44051fbf60
YoussefNesafe <ynessafe@gmail.com>
2023-04-06 17:07:09 +0400

    navabr

---
+++ src/components/Navbar/navbarContent.js
@@ -0,0 +1,22 @@
+export const NAVBAR_CONTENT = [
+	{
+		url: "#home",
+		title: "Home",
+	},
+	{
+		url: "#about",
+		title: "About",
+	},
+	{
+		url: "#experience",
+		title: "Experience",
+	},
+	{
+		url: "#projects",
+		title: "Projects",
+	},
+	{
+		url: "#contact",
+		title: "Contact",
+	},
+];
