03d8eb655a2e35b59da2cc0289908861c6650e95
YoussefNesafe <ynessafe@gmail.com>
2023-04-08 04:52:55 +0400

    add: contact page

---
+++ src/ContentData/contactContent.js
@@ -0,0 +1,10 @@
+export const CONTACT_CONTENT = {
+	titleSection: {
+		number: "04",
+		title: "What's Next",
+	},
+	mainTitle: "Get In Touch",
+	describtion:
+		"Although I’m not currently looking for any new opportunities, my inbox is always open. Whether you have a question or just want to say hi, I’ll try my best to get back to you!",
+	sayHiBtn: "Say Hello",
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import Navbar from "@/components/Navbar";
 import Experience from "@/components/Experience";
 import Head from "next/head";
 import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
 
 export default function Home() {
 	return (
@@ -26,6 +27,7 @@ export default function Home() {
 						<About />
 						<Experience />
 						<Projects />
+						<Contact />
 					</MainSection>
 					<EmailSection />
 				</div>
