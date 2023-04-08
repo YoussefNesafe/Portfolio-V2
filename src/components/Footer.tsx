d0909590ea284500ad7fd0132eb5e7773930aac5
YoussefNesafe <ynessafe@gmail.com>
2023-04-08 04:59:21 +0400

    fix: responsive issue

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { SOCIAL_MEDIA_LINKS } from "./MainLayout/MainLayoutContent";
 
 const Footer = () => {
 	return (
-		<div className="inline-flex items-center justify-center w-full gap-4 py-6 xl:hidden">
+		<div className="items-center justify-center hidden w-full gap-4 py-6 mdl:inline-flex xl:hidden">
 			{SOCIAL_MEDIA_LINKS.map((link) => (
 				<a href={link?.url} target="_blank" key={link.url}>
 					<span className="inline-flex items-center justify-center w-10 h-10 text-xl transition-all duration-300 rounded-full cursor-pointer bg-hoverColor hover:text-textGreen hover:-translate-y-2">
